754d15ee3f13852936cdc3f1db11b220155fd4ae
Sarvesh Mishra <sarvesh.m@carwale.com>
2022-06-25 10:04:01 +0530

    Add car model

---
+++ Controllers/CarModelController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using net_rest_api.Models.CarModel;
+using net_rest_api.Repository;
+
+namespace net_rest_api.Controllers
+{
+    // For simplicity for frontend apps
+    [ApiController]
+    [Route("cars")]     // For defining own route name. If don't define controller name will be route name
+    public class CarModelController : ControllerBase
+    {
+
+        private readonly CarRepository _repository;
+
+        public CarModelController()
+        {
+            _repository = new CarRepository();
+
+        }
+
+        // GET /cars
+        [HttpGet]
+        public IEnumerable<CarModel> GetCars()
+        {
+
+            var cars = _repository.GetCarModels();
+            return cars;
+
+        }
+
+
+
+    }
+}
--- Repository/CarRepository.cs
+++ Repository/CarRepository.cs
@@ -1,7 +1,23 @@
+using System.Linq;
+using net_rest_api.Models.CarModel;
+
 namespace net_rest_api.Repository
 {
     public class CarRepository
     {
-        
+        private readonly List<CarModel> _carModels = new()
+        {
+            new CarModel{id= Guid.NewGuid(), Name="Honda", ModelName="City", CreatedAt= DateTime.Now},
+
+            new CarModel{id= Guid.NewGuid(), Name="Maruti Suzaki", ModelName="Alto", CreatedAt= DateTime.Now},
+
+            new CarModel{id= Guid.NewGuid(), Name="BMW", ModelName="M3", CreatedAt= DateTime.Now}
+       };
+        // using arrow function similar to js
+
+        // Return all car models in repository.
+        public IEnumerable<CarModel> GetCarModels() => _carModels;
+
+        public CarModel? GetCar(Guid id) => _carModels.Where(carModels => carModels.id == id).SingleOrDefault();
     }
 }
